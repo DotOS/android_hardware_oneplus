31aba9a96200af0521cf5153d8a1ad37ccba5bc0
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-20 00:30:34 +0900

    sm8150-common: fod: Set thread to high CFS priority
    Change-Id: I83eab988551892b9229640f601e17f7235d133ed
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- hidl/powershare/service.cpp
+++ hidl/powershare/service.cpp
@@ -33,6 +33,7 @@ using android::status_t;
 int main() {
     android::sp<IPowerShare> service = new PowerShare();
 
+    android::hardware::setMinSchedulerPolicy(service, SCHED_NORMAL, -20);
     configureRpcThreadpool(1, true);
 
     status_t status = service->registerAsService();
